15d9e58e0313e46820b5bded6f1faa2afc4b97ab
Shashank Yadav <shashankyadav724@gmail.com>
2025-10-21 20:36:26 +0530

    secure api

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
 export const YT_URL="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=in&key="
-export const YT_Search_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YT_Search_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 export const YT_Search_Result = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&key=${import.meta.env.VITE_API_KEY}&q=`
 export const comments = [
     {
